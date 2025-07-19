4f3606577e436e803cc0ebbcc88560e0bc6bc417
Deniz Baines <168414213+DenizBaines@users.noreply.github.com>
2025-07-19 22:20:04 +0530

    Add files via upload

--- 1233 - Remove Sub-Folders from the Filesystem.cpp
+++ 1233 - Remove Sub-Folders from the Filesystem.cpp
@@ -1,29 +1,18 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+
 class Solution {
 public:
-    vector<string> removeSubfolders(vector<string>& folder) {
-        // Sort the folders lexicographically so parent folders come before their subfolders
-        sort(folder.begin(), folder.end());
+    std::vector<std::string> removeSubfolders(std::vector<std::string>& folder) {
+        std::sort(folder.begin(), folder.end());
+        std::vector<std::string> ans = {folder[0]};
 
-        // Initialize result vector with the first folder
-        vector<string> ans;
-        ans.push_back(folder[0]);
-
-        // Iterate through remaining folders starting from index 1
-        for(int i = 1; i < folder.size(); i++) {
-            // Get the last added folder path and add a trailing slash
-            // This helps in comparing if current folder is a subfolder
-            string lastFolder = ans.back();
-            lastFolder.push_back('/');
-
-            // Compare current folder with last added folder
-            // compare(0, lastFolder.size(), lastFolder) checks if folder[i] starts with lastFolder
-            // If it doesn't start with lastFolder (returns != 0), then it's not a subfolder
-            if(folder[i].compare(0, lastFolder.size(), lastFolder) != 0) {
-                // If not a subfolder, add to result
+        for (int i = 1; i < folder.size(); ++i) {
+            if (folder[i].substr(0, ans.back().size() + 1) != ans.back() + "/") {
                 ans.push_back(folder[i]);
             }
         }
-
         return ans;
     }
 };
