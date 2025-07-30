6b640f0ae0f34ac136f62cf1d02d48e0b1c3131a
Deniz Baines <168414213+DenizBaines@users.noreply.github.com>
2025-07-30 10:10:00 +0530

    Add files via upload

--- 2419 - Longest Subarray With Maximum Bitwise AND.cpp
+++ 2419 - Longest Subarray With Maximum Bitwise AND.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int len = 1;
-
-        // step 01
-        int mx = *max_element(nums.begin(),nums.end()); //O(n)
-
-        // step 02
-        int tmp_len=0;
-        for(int i=0;i<nums.size();i++){ // O(n)
-            if(nums[i]==mx){
-                tmp_len++; // 3
-            }
-            else{
-                len = max(len, tmp_len);
-                tmp_len=0;
+        int maxVal = *max_element(nums.begin(), nums.end());
+        int streak = 0, maxStreak = 0;
+        for (int num : nums) {
+            if (num == maxVal) {
+                streak++;
+                if (streak > maxStreak) maxStreak = streak;
+            } else {
+                streak = 0;
             }
         }
-
-        len = max(len, tmp_len);
-
-        return len;
+        return maxStreak;
     }
 };
